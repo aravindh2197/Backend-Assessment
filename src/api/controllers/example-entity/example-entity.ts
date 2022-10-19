1c41e86a187963fff9b4bd306c07ae74269419d9
aravindh2197 <arp2197@gmail.com>
2022-10-18 22:41:11 -0400

    initial commit

---
+++ src/api/controllers/example-entity/example-entity.ts
@@ -0,0 +1,14 @@
+import { JsonController } from "routing-controllers";
+
+@JsonController("/example-entity")
+export class UserController {
+//   @Get()
+//   get(): Promise<ExampleEntity[]> {
+//     return ExampleEntity.find();
+//   }
+
+//   @Post()
+//   create(@Body() body: Pick<ExampleEntity, "exampleColumn">): Promise<ExampleEntity> {
+//     return ExampleEntity.create(body).save();
+//   }
+}
