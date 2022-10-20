9b0d1911e7ae339bfb9bf84e192c9848210370b1
aravindh2197 <arp2197@gmail.com>
2022-10-19 22:06:19 -0400

    car details data model

--- src/api/models/car-details/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./car-details";
-export * from "./car-details.model";
-export * from "./car-details-status.enum";
-export * from "./car-details.input";
-export * from "./car-color.enum";
